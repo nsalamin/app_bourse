652586121b0732a14dc284f47688fb04309cec61
Nicolas Salamin <nicolas.salamin@unil.ch>
2025-05-11 22:05:56 +0200

    changed the python script to avoid loosing the first line of the file to merge

--- merge_csv.py
+++ merge_csv.py
@@ -9,9 +9,10 @@ shutil.copy(standardfile, "vendeur_prix_old.csv")
 
 max_buyer = 0 #higher id of buyer column (index 2)
 with open("vendeur_prix_old.csv", "r") as infile, open("vendeur_prix.csv", "w") as outfile:
-    line = infile.readline() #read the header, same for both
-    outfile.write(line) #write the header to the new file
     for line in infile:
+        if line.startswith("id"):
+            outfile.write(line)
+            continue
         data = line.split(",")
         col2 = int(data[2])
         if col2 > max_buyer:
@@ -19,8 +20,9 @@ with open("vendeur_prix_old.csv", "r") as infile, open("vendeur_prix.csv", "w") 
         outfile.write(",".join(data))  # write the joined data as a line
 
 with open(otherfile, "r") as tomerge, open("vendeur_prix.csv", "a") as outfile:
-    line = tomerge.readline() #same as above
     for line in tomerge:
+        if line.startswith("id"):
+            continue
         data = line.split(",")
         data[2] = str(int(data[2]) + max_buyer) #add to the buyer id the max of the previous data
         outfile.write(",".join(data))
